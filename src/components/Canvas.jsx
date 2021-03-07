327f5eb69eedce4a575cb4f6d15a58a328ed813f
ogneyar <ogneyar-ne@ya.ru>
2021-03-07 22:49:03 +0300

    test

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import { observer } from 'mobx-react-lite'
+import React, { useEffect } from 'react'
+import canvasState from '../store/canvasState'
 import '../styles/canvas.scss'
 
-const Canvas = () => {
+
+const Canvas = observer(() => {
+    useEffect(() => {
+        canvasState.setCanvas()
+    }, [])
+
     return (
         <div className="canvas">
             <canvas width={600} height={400} />
         </div>
     )
-}
+})
 
 export default Canvas
