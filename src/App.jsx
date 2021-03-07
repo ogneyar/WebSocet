ab9ea72372ea6515a39dd38adf3723b5c1108056
ogneyar <ogneyar-ne@ya.ru>
2021-03-07 21:43:16 +0300

    edit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles/app.scss'
+import Toolbar from './components/Toolbar'
+import Settingbar from './components/Settingbar'
+import Canvas from './components/Canvas'
+
+function App() {
+  return (
+    <div className="App">
+      <Toolbar />
+      <Settingbar />
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Settingbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/settingbar.scss'
+
+const Settingbar = () => {
+    return (
+        <div className="settingbar">
+            
+        </div>
+    )
+}
+
+export default Settingbar
